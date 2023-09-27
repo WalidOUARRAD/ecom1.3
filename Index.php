28fd31cc4c04b51214e3ec599335f9413e6312cd
unknown <walid67ouarrad@gmail.com>
2023-09-27 17:55:30 -0400

    add form and result page

---
+++ Index.php
@@ -0,0 +1,12 @@
+<!Doctype html>
+<html>
+    <body>
+
+<form method="post" action="result.php">
+    name :
+    <input type="text" name="fname">
+    <input type="submit">
+
+</form>
+    </body>
+</html>
